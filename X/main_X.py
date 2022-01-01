cac050067482d2882ba69c80e9bcd535168242ae
pengu5055 <urbancmarko1@gmail.com>
2022-01-01 16:50:52 +0100

    Ionization current plot

--- X/main_X.py
+++ X/main_X.py
@@ -23,14 +23,17 @@ c1, c2, c3 = cmr.take_cmap_colors("cmr.cosmic", 3, cmap_range=(0.3, 1), return_f
 plt.plot(U_25, I_25, alpha=0.3, c=c1)
 plt.errorbar(U_25, I_25, yerr=I_25_err,  markersize=2, color=c1,
              linestyle='None', marker="o", capsize=2, label=r"25 kV", alpha=1)
+plt.axhline(y=1.18e-9, color="#adadad", ls="--", alpha=0.5)
 plt.plot(U_30, I_30, alpha=0.3, c=c2)
 plt.errorbar(U_30, I_30, yerr=I_30_err,  markersize=2, color=c2,
              linestyle='None', marker="o", capsize=2, label=r"30 kV", alpha=1)
+plt.axhline(y=2.175e-9, color="#adadad", ls="--", alpha=0.5)
 plt.plot(U_35, I_35, alpha=0.3, c=c3)
 plt.errorbar(U_35, I_35, yerr=I_35_err,  markersize=2, color=c3,
              linestyle='None', marker="o", capsize=2, label=r"25 kV", alpha=1)
+plt.axhline(y=3.36e-9, color="#adadad", ls="--", alpha=0.5)
 
-plt.title("Merjenje toka nasišenja v ionizacijski celici ")
+plt.title("Merjenje toka nasičenja v ionizacijski celici")
 plt.xlabel(r"$U_s$ [V]")
 plt.ylabel(r"$I_e$ [A]")
 
