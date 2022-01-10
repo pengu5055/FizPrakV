1b52b2a1930fe246468bc5781a7b88095a5211b9
pengu5055 <urbancmarko1@gmail.com>
2022-01-11 00:09:35 +0100

    Final commit

--- NMR/main_NMR.py
+++ NMR/main_NMR.py
@@ -5,7 +5,7 @@ from scipy.optimize import curve_fit
 
 
 def exp(x, A, tau):
-    return A*np.exp(-x/tau)
+    return A*np.exp(-2*x/tau)
 
 
 def exp2(x, A, tau):
@@ -24,7 +24,7 @@ fitpar, fitcov = curve_fit(exp, t1, u1, p0=[4, 3])
 fit = exp(t1, *fitpar)
 
 fig, ax = plt.subplots()
-fittext = "Exp. fit: $y = U_0exp(-t/T_2)$\n$U_0$ = {} ± {}\n$T_2$ = {} ± {}"\
+fittext = "Exp. fit: $y = U_0exp(-2t/T_2)$\n$U_0$ = {} ± {}\n$T_2$ = {} ± {}"\
     .format(format(fitpar[0], ".4e"), format(fitcov[0][0]**0.5, ".4e"),
             format(fitpar[1], ".4e"), format(fitcov[1][1]**0.5, ".4e"))
 plt.text(0.5, 0.55, fittext, ha="left", va="center", size=10, transform=ax.transAxes,
@@ -42,12 +42,12 @@ plt.show()
 
 # Ion water T1 plot
 data3_T1 = np.column_stack(np.genfromtxt("data_glT1_ion.csv", delimiter=",", skip_header=1))
-t1 = data3_T1[0]
+t1 = data3_T1[0] / 25
 u1 = data3_T1[1]
 u1_err = [0.03 * i for i in u1]
 
 fitpar, fitcov = curve_fit(exp2, t1, u1, p0=[4.39, 2.29])
-fit = exp2(np.linspace(0, 500, 2000), *fitpar)
+fit = exp2(np.linspace(0, 20, 2000), *fitpar)
 
 fig, ax = plt.subplots()
 fittext = "Exp. fit: $y = U_0(1 - exp(-t/T_1))$\n$U_0$ = {} ± {}\n$T_1$ = {} ± {}"\
@@ -58,22 +58,22 @@ plt.text(0.5, 0.55, fittext, ha="left", va="center", size=10, transform=ax.trans
 
 plt.errorbar(t1, u1, yerr=u1_err,  markersize=2, color=c2,
              linestyle='None', marker="o", capsize=2, alpha=1, label="Meritve")
-plt.plot(np.linspace(0, 500, 2000), fit, c=c3)
+plt.plot(np.linspace(0, 20, 2000), fit, c=c3)
 
 plt.title("Signal proste precesije ion water")
 plt.xlabel(r"$\tau$ [ms]")
-plt.ylabel("U [mV]")
+plt.ylabel("U [V]")
 plt.legend()
 plt.show()
 
 # Tap water T1 plot
 data4_T1 = np.column_stack(np.genfromtxt("data_glT1_tap.csv", delimiter=",", skip_header=1))
-t1 = data4_T1[0]
+t1 = data4_T1[0] / 20
 u1 = data4_T1[1]
 u1_err = [0.03 * i for i in u1]
 
 fitpar, fitcov = curve_fit(exp2, t1, u1, p0=[1, 1])
-fit = exp2(np.linspace(0, 100, 2000), *fitpar)
+fit = exp2(np.linspace(0, 5, 2000), *fitpar)
 
 fig, ax = plt.subplots()
 fittext = "Exp. fit: $y = U_0(1 - exp(-t/T_1))$\n$U_0$ = {} ± {}\n$T_1$ = {} ± {}"\
@@ -84,10 +84,10 @@ plt.text(0.5, 0.35, fittext, ha="left", va="center", size=10, transform=ax.trans
 
 plt.errorbar(t1, u1, yerr=u1_err,  markersize=2, color=c2,
              linestyle='None', marker="o", capsize=2, alpha=1, label="Meritve")
-plt.plot(np.linspace(0, 100, 2000), fit, c=c3)
+plt.plot(np.linspace(0, 5, 2000), fit, c=c3)
 
 plt.title("Signal proste precesije tap water")
 plt.xlabel(r"$\tau$ [s]")
-plt.ylabel("U [mV]")
+plt.ylabel("U [V]")
 plt.legend()
 plt.show()
